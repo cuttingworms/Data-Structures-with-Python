8c9455c393e403db553f7868cba90a266bc4b56b
Seungwon Bae <21storeo@gmail.com>
2021-12-27 21:36:28 +0900

    stack using python list

---
+++ stack & queue/stack_by_python_list.py
@@ -0,0 +1,16 @@
+stack = []
+
+
+def push(item):
+    stack.append(item)
+
+    
+def peek():
+    if len(stack) != 0:
+        return stack[-1]
+
+
+def pop():
+    if len(stack) != 0:
+        item = stack.pop(-1)
+        return item
