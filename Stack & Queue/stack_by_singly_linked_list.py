4822b054f209418a51a04cb8a12ef557c50f2cfa
Seungwon Bae <21storeo@gmail.com>
2021-12-27 21:59:08 +0900

    stack using slist

---
+++ Stack & Queue/stack_by_singly_linked_list.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, item, link):
+        self.item = item
+        self.next = link
+
+
+def push(item):
+    global top
+    global size
+    top = Node(item, top)
+    size += 1
+    
+
+def peek():
+    if size != 0:
+        return top.item
+    
+
+def pop():
+    global top
+    global size
+    
+    if size != 0:
+        top_item = top.item
+        top = top.next
+        size -= 1
+        return top_item
+    
+
+def print_stack():
+    print("top ->\t", end="")
+    p = top
+    
+    while p:
+        if p.next != None:
+            print(p.item, " -> ", end="")
+        else:
+            print(p.item, end="")
+        p = p.next
+        
+        
+top = None
+size = 0        
